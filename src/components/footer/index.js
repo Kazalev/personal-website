8422d2f76d51e8de8648dec960eff7c0b01ce880
Kristian Kazalev <kristian.kazalev@gmail.com>
2020-10-22 15:04:06 +0300

    footer and sidenav adjusted

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -3,8 +3,10 @@ import styles from "./index.module.css";
 
 const Footer = () => {
     return (
-        <div className="footer" id="footer">
-            <p className={styles.p}>Footer &copy; <span id="year">{new Date().getFullYear()}</span> by Kristian Kazalev for ReactJS Course</p>
+        <div className="fixed-bottom">
+            <div className={styles.footer} id="footer">
+                <p className={styles.p}>Footer &copy; <span id="year">{new Date().getFullYear()}</span> by Kristian Kazalev for ReactJS Course</p>
+            </div>
         </div>
     );
 };
--- src/components/sidenav/index.js
+++ src/components/sidenav/index.js
@@ -1,14 +1,34 @@
 import React from "react";
 import styles from "./index.module.css";
-import { Container, Row, Col, Image } from "react-bootstrap";
+import { Image } from "react-bootstrap";
 
 
 const Sidenav = () => {
     return (
         <div className={styles.sidenav} id="sidenav">
             <a className={styles.a}>
-                <Image src="../../me.jpg" height="170" width="165" roundedCircle className={styles.border} />
+                <Image src="../../me.jpg" height="200" width="170" rounded className={styles.border} />
             </a>
+            <ul>
+                <li>
+                    <a href="#">About</a>
+                </li>
+                <li>
+                    <a href="#education">Education</a>
+                </li>
+                <li>
+                    <a href="#experience">Experience</a>
+                </li>
+                <li>
+                    <a href="#skills">Skills</a>
+                </li>
+                <li>
+                    <a href="#certifications">Certifications</a>
+                </li>
+                <li>
+                    <a href="#projects">Projects</a>
+                </li>
+            </ul>
         </div>
     );
 };
